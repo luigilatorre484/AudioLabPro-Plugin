51396cb2ef0da92215092bf71c9e0219fa7fee27
luigilatorre484 <luigilatorre484@gmail.com>
2026-05-01 11:09:47 +0200

    Update PluginEditor.cpp

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,11 +1,11 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "BinaryData.h" // <--- IMPORTANTE: Senza questo BinaryData non viene visto
 
 //==============================================================================
 AudioLabProAudioProcessorEditor::AudioLabProAudioProcessorEditor (AudioLabProAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Imposta le dimensioni della finestra del plugin
     setSize (1000, 600);
 }
 
@@ -16,7 +16,7 @@ AudioLabProAudioProcessorEditor::~AudioLabProAudioProcessorEditor()
 //==============================================================================
 void AudioLabProAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // Carica l'immagine console.png dalla memoria binaria
+    // Carica l'immagine console.png
     auto backgroundImage = juce::ImageCache::getFromMemory (BinaryData::console_png, BinaryData::console_pngSize);
     
     if (backgroundImage.isValid())
@@ -32,5 +32,4 @@ void AudioLabProAudioProcessorEditor::paint (juce::Graphics& g)
 
 void AudioLabProAudioProcessorEditor::resized()
 {
-    // Qui andranno posizionati i cursori e i pulsanti in futuro
 }
