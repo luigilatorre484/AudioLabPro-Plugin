db685be0ace633fd9a228c457298209b849cfc83
luigilatorre484 <luigilatorre484@gmail.com>
2026-05-01 10:56:45 +0200

    Update PluginProcessor.h

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <juce_audio_processors/juce_audio_processors.h>
-#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_dsp/juce_dsp.h> // Indispensabile per FFT
+
+// ... resto del codice ...
 #include <juce_gui_basics/juce_gui_basics.h>
 #include <vector>
 
